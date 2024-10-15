9ad9c3c46f116639f5b9435dc48a16c3f4bf5377
Micka <mickael.murmann@gmail.com>
2024-10-15 11:46:42 +0200

    Fix CSS (Navbar)

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
 
     return (
         <nav className={`bg-slate-200 dark:bg-gray-800 shadow-md w-full z-10 py-5`}>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="px-4 sm:px-6 lg:px-20">
                 <div className="flex items-center justify-between h-16">
                     {/* Logo */}
                     <div className="flex items-center">
