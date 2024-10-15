aa474c1947f0c09f24f3ce75c013da5a93b6f2a6
Micka <mickael.murmann@gmail.com>
2024-10-15 14:16:50 +0200

    Fix CSS (Search bar dark / light mode)

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -71,10 +71,11 @@ export default function CourseList() {
                     <input
                         type="text"
                         placeholder="Rechercher un cours..."
-                        className={`border rounded-lg px-4 py-2 w-full 
-                            ${loading ? 'border-gray-300' : 'border-gray-600'} 
-                            ${loading ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-200'} 
-                            focus:outline-none focus:ring-2 focus:ring-blue-500`}
+                        className={`border rounded-lg px-4 py-2 w-full
+                            border-gray-300 bg-white text-gray-900 
+                            dark:border-gray-600 dark:bg-slate-800 dark:text-gray-200
+                            focus:outline-none focus:ring-2 focus:ring-blue-500
+                            dark:focus:ring-blue-400`}
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)} // Met à jour l'état de recherche
                     />
