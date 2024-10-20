0ddc254af2e8b6b599ba3f7bb965a4bdcbeddebf
Micka <mickael.murmann@gmail.com>
2024-10-20 23:45:43 +0200

    Optimize pages (courses and progression)

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -1,65 +1,56 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import { Course } from '@/types/types';
 import CourseCard from './_components/CourseCard';
 import Pagination from './[courseId]/_components/Pagination';
 import { useUser } from '@clerk/nextjs';
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 
 export default function CourseList() {
     const [courses, setCourses] = useState<Course[]>([]);
     const [loading, setLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
-    const [coursesPerPage] = useState(6); 
+    const [coursesPerPage] = useState(6);
     const [searchTerm, setSearchTerm] = useState('');
-
+    
     const router = useRouter();
     const { isSignedIn, user } = useUser();
 
-    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentPage(1); // Reset to the first page when the search term changes
-        setSearchTerm(e.target.value);
-    };
-
-    let isAdmin = false;
-    if (user) {
-        isAdmin = user.publicMetadata?.role === 'admin'; // Optional chaining to prevent errors
-    }
+    const isAdmin = user?.publicMetadata?.role === 'admin';
 
     useEffect(() => {
-        async function fetchCourses() {
+        const fetchCourses = async () => {
             try {
-                const response = await fetch('/api/course', {
-                    method: 'GET',
-                });
-
-                if (response.ok) {
-                    const data = await response.json();
-                    setCourses(data);
-                } else {
-                    console.error('Erreur lors de la récupération des cours.');
-                }
+                const response = await fetch('/api/course');
+                if (!response.ok) throw new Error('Erreur lors de la récupération des cours.');
+                const data = await response.json();
+                setCourses(data);
             } catch (error) {
                 console.error('Erreur lors de la récupération des cours :', error);
             } finally {
                 setLoading(false);
             }
-        }
-
+        };
         fetchCourses();
     }, []);
 
-    const filteredCourses = courses.filter(course =>
-        course.title.toLowerCase().includes(searchTerm.toLowerCase())
+    // Utilisation de useMemo pour le filtrage des cours
+    const filteredCourses = useMemo(() => 
+        courses.filter(course =>
+            course.title.toLowerCase().includes(searchTerm.toLowerCase())
+        ), [courses, searchTerm]
     );
 
     // Calcul des cours à afficher pour la page actuelle
     const indexOfLastCourse = currentPage * coursesPerPage;
     const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
-    const currentCourses = filteredCourses.slice(indexOfFirstCourse, indexOfLastCourse);
+    const currentCourses = useMemo(() => 
+        filteredCourses.slice(indexOfFirstCourse, indexOfLastCourse), 
+        [filteredCourses, indexOfFirstCourse, indexOfLastCourse]
+    );
 
     // Calcul du nombre total de pages
     const totalPages = Math.ceil(filteredCourses.length / coursesPerPage);
@@ -74,30 +65,27 @@ export default function CourseList() {
     return (
         <div className="min-h-screen py-8 bg-gray-100 dark:bg-gray-900">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-8">
-                    Cours
-                </h1>
+                <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-8">Cours</h1>
 
                 {/* Barre de recherche */}
                 <div className="mb-4">
                     <input
                         type="text"
                         placeholder="Rechercher un cours..."
-                        className={`border rounded-lg px-4 py-2 w-full
-                            border-gray-300 bg-white text-gray-900 
-                            dark:border-gray-600 dark:bg-slate-800 dark:text-gray-200
-                            focus:outline-none focus:ring-2 focus:ring-blue-500
-                            dark:focus:ring-blue-400`}
+                        className="border rounded-lg px-4 py-2 w-full border-gray-300 bg-white text-gray-900 dark:border-gray-600 dark:bg-slate-800 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400"
                         value={searchTerm}
-                        onChange={handleSearchChange} // Met à jour l'état de recherche
+                        onChange={(e) => {
+                            setCurrentPage(1); // Reset to the first page when the search term changes
+                            setSearchTerm(e.target.value);
+                        }}
                     />
                 </div>
 
-                {(isSignedIn && isAdmin) && (
-                    
+                {/* Bouton d'ajout de cours visible uniquement pour les admins */}
+                {isSignedIn && isAdmin && (
                     <div className="mb-8">
                         <button
-                            onClick={() => router.push(`/admin/course`)} // Redirige vers la page de création de cours
+                            onClick={() => router.push(`/admin/course`)}
                             className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors"
                         >
                             Ajouter un cours
--- app/progression/page.tsx
+++ app/progression/page.tsx
@@ -1,29 +1,29 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { useUser } from '@clerk/nextjs';
-import { LessonProgress } from '@/types/types';
-import { LibraryBigIcon, Eye } from 'lucide-react';
+import { useEffect, useState, useMemo, useCallback } from "react";
+import Link from "next/link";
+import { useUser } from "@clerk/nextjs";
+import { LessonProgress } from "@/types/types";
+import { LibraryBigIcon, Eye } from "lucide-react";
 
 const UserProgressions = () => {
-    const { user } = useUser(); 
+    const { user } = useUser();
     const [progressions, setProgressions] = useState<LessonProgress[]>([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchProgressions = async () => {
-            if (!user) return; 
+            if (!user) return;
 
             try {
                 const response = await fetch(`/api/user/progress`);
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                throw new Error("Network response was not ok");
                 }
                 const data = await response.json();
                 setProgressions(data);
             } catch (error) {
-                console.error('Error fetching progressions:', error);
+                console.error("Error fetching progressions:", error);
             } finally {
                 setLoading(false);
             }
@@ -32,25 +32,12 @@ const UserProgressions = () => {
         fetchProgressions();
     }, [user]);
 
-    if (loading) return <p className="text-gray-500 text-center dark:text-gray-400 mt-5">Chargement...</p>;
-
-    const getProgressColor = (progress: number) => {
-        if (progress === 100) return 'bg-green-500'; // Complété
-        if (progress >= 75) return 'bg-green-400'; // 75% et plus
-        if (progress >= 50) return 'bg-yellow-400'; // 50% à 74%
-        if (progress >= 25) return 'bg-orange-400'; // 25% à 49%
-        return 'bg-red-400'; // Moins de 25%
-    };
-
-    // Regrouper les progressions par cours
-    const groupByCourse = (progressions: LessonProgress[]) => {
+    // Utilisez useMemo pour optimiser le regroupement des progressions par cours
+    const progressionsByCourse = useMemo(() => {
         return progressions.reduce((acc, progress) => {
             const courseTitle = progress.lesson.course.title;
             if (!acc[courseTitle]) {
-                acc[courseTitle] = {
-                    inProgress: [],
-                    completed: [],
-                };
+                acc[courseTitle] = { inProgress: [], completed: [] };
             }
             if (progress.completed) {
                 acc[courseTitle].completed.push(progress);
@@ -58,12 +45,20 @@ const UserProgressions = () => {
                 acc[courseTitle].inProgress.push(progress);
             }
             return acc;
-        }, {} as Record<string, { inProgress: LessonProgress[], completed: LessonProgress[] }>);
-    };
+        }, {} as Record<string, { inProgress: LessonProgress[]; completed: LessonProgress[] }>);
+    }, [progressions]);
 
-    const progressionsByCourse = groupByCourse(progressions);
+    // Utilisez useCallback pour mémoriser les fonctions constantes
+    const getProgressColor = useCallback((progress: number) => {
+        if (progress === 100) return "bg-green-500";
+        if (progress >= 75) return "bg-green-400";
+        if (progress >= 50) return "bg-yellow-400";
+        if (progress >= 25) return "bg-orange-400";
+        return "bg-red-400";
+    }, []);
 
-    const renderTable = (chapters: LessonProgress[]) => (
+    const renderTable = useCallback(
+        (chapters: LessonProgress[]) => (
         <table className="text-center min-w-full table-fixed bg-slate-100 dark:bg-gray-800 rounded-lg overflow-hidden shadow hidden md:table mt-5">
             <thead className="bg-gray-200 dark:bg-gray-700">
                 <tr>
@@ -74,99 +69,106 @@ const UserProgressions = () => {
                 </tr>
             </thead>
             <tbody className="divide-y divide-gray-200 dark:divide-gray-600">
-                {chapters.map((progress) => (
-                    <tr key={progress.id} className="hover:bg-gray-100 dark:hover:bg-gray-700/30 transition">
-                        <td className="text-left py-5 px-4 text-gray-700 dark:text-gray-300">
-                                {progress.lesson.title}
-                            <div className="progress-bar w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700 mt-2">
+            {chapters.map((progress) => (
+                <tr key={progress.id} className="hover:bg-gray-100 dark:hover:bg-gray-700/30 transition">
+                    <td className="text-left py-5 px-4 text-gray-700 dark:text-gray-300">
+                        {progress.lesson.title}
+                        <div className="progress-bar w-full h-2 bg-gray-200 rounded-full dark:bg-gray-700 mt-2">
                                 <div
                                     className={`progress h-full ${getProgressColor(Math.min(progress.progress, 100))} rounded-full`}
                                     style={{ width: `${Math.min(progress.progress, 100)}%` }}
                                 ></div>
-                            </div>
-                        </td>
-                        <td className="py-2 px-4 text-gray-600 dark:text-gray-400">
-                            {Math.min(parseFloat(progress.progress.toFixed(2)), 100).toFixed(0)} %
-                        </td>
-                        <td className={`py-2 px-4 text-sm ${progress.completed ? 'text-green-500' : 'text-yellow-500'} dark:${progress.completed ? 'text-green-300' : 'text-yellow-400'}`}>
-                            {progress.completed ? 'Terminé' : 'En cours'}
-                        </td>
-                        <td className="py-2 px-4 text-center">
-                            <Link href={`/lesson/${progress.lesson.id}`} className="dark:text-white flex justify-center items-center h-full">
-                                <Eye size={20} />
-                            </Link>
-                        </td>
-                    </tr>
-                ))}
+                        </div>
+                    </td>
+                    <td className="py-2 px-4 text-gray-600 dark:text-gray-400">
+                        {Math.min(progress.progress, 100).toFixed(0)} %
+                    </td>
+                    <td className={`py-2 px-4 text-sm ${progress.completed ? "text-green-500" : "text-yellow-500"}`}>
+                        {progress.completed ? "Terminé" : "En cours"}
+                    </td>
+                    <td className="py-2 px-4 text-center">
+                        <Link href={`/lesson/${progress.lesson.id}`} className="dark:text-white flex justify-center items-center h-full">
+                        <Eye size={20} />
+                        </Link>
+                    </td>
+                </tr>
+            ))}
             </tbody>
         </table>
+        ),
+        [getProgressColor]
     );
 
-    // Responsive version for small screens
-    const renderResponsive = (chapters: LessonProgress[]) => (
+    const renderResponsive = useCallback(
+        (chapters: LessonProgress[]) => (
         <div className="md:hidden">
             {chapters.map((progress) => (
-                <div key={progress.id} className="mb-4 p-4 bg-gray-100 dark:bg-gray-700/50 dark:hover:bg-gray-700/60 rounded-lg shadow">
-                    <Link className="dark:text-white" href={`/lesson/${progress.lesson.id}`}>
-                        {progress.lesson.title}
-                    </Link>
-                    <div className="progress-bar w-full h-2 bg-gray-200 rounded-full dark:bg-gray-600 mt-2">
-                        <div
-                            className={`progress h-full ${getProgressColor(Math.min(progress.progress, 100))} rounded-full`}
-                            style={{ width: `${Math.min(progress.progress, 100)}%` }}
-                        ></div>
-                    </div>
-                    <p className="mt-2 text-gray-600 dark:text-gray-400">
-                        Progression : {Math.min(parseFloat(progress.progress.toFixed(2)), 100).toFixed(0)} %
-                    </p>
-                    <p className={`text-sm ${progress.completed ? 'text-green-500' : 'text-yellow-500'} dark:${progress.completed ? 'text-green-300' : 'text-yellow-400'}`}>
-                        {progress.completed ? 'Terminé' : 'En cours'}
-                    </p>
-                    <Link href={`/lesson/${progress.lesson.id}`} className="p-3 text-white bg-emerald-500 hover:bg-emerald-600 mt-5 rounded-lg inline-flex justify-center items-center h-full transition duration-300">
-                        <Eye size={20} />
-                    </Link>
+            <div key={progress.id} className="mb-4 p-4 bg-gray-100 dark:bg-gray-700/50 dark:hover:bg-gray-700/60 rounded-lg shadow">
+                <Link className="dark:text-white" href={`/lesson/${progress.lesson.id}`}>
+                    {progress.lesson.title}
+                </Link>
+                <div className="progress-bar w-full h-2 bg-gray-200 rounded-full dark:bg-gray-600 mt-2">
+                    <div
+                        className={`progress h-full ${getProgressColor(Math.min(progress.progress, 100))} rounded-full`}
+                        style={{ width: `${Math.min(progress.progress, 100)}%` }}
+                    ></div>
                 </div>
+                <p className="mt-2 text-gray-600 dark:text-gray-400">Progression : {Math.min(progress.progress, 100).toFixed(0)} %</p>
+                <p className={`text-sm ${progress.completed ? "text-green-500" : "text-yellow-500"}`}>
+                    {progress.completed ? "Terminé" : "En cours"}
+                </p>
+                <Link href={`/lesson/${progress.lesson.id}`} className="p-3 text-white bg-emerald-500 hover:bg-emerald-600 mt-5 rounded-lg inline-flex justify-center items-center h-full transition duration-300">
+                    <Eye size={20} />
+                </Link>
+            </div>
             ))}
         </div>
+        ),
+        [getProgressColor]
     );
+
+    if (loading) {
+        return <p className="text-gray-500 text-center dark:text-gray-400 mt-5">Chargement...</p>;
+    }
 
     return (
         <div className="px-0 py-4 md:p-4">
             <div className="mx-auto p-3 md:p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
                 <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Progressions des chapitres par cours</h2>
                 <div className="mt-4 overflow-x-auto">
-                    {Object.keys(progressionsByCourse).map((courseTitle) => (
-                        <div key={courseTitle} className="mb-8 border dark:border-slate-600 p-6 rounded-lg">
-                            {(progressionsByCourse[courseTitle].inProgress.length > 0 || progressionsByCourse[courseTitle].completed.length > 0) && (
-                                <Link className="flex gap-2 items-center" 
-                                    href={`/course/${
-                                        progressionsByCourse[courseTitle].inProgress[0]?.lesson.course.id ||
-                                        progressionsByCourse[courseTitle].completed[0]?.lesson.course.id
-                                    }`}>
-                                    <LibraryBigIcon className="dark:text-white" size={20} />
-                                    <span className="font-bold text-2xl dark:text-white">{courseTitle}</span>
-                                </Link>
-                            )}
+                {Object.keys(progressionsByCourse).map((courseTitle) => (
+                    <div key={courseTitle} className="mb-8 border dark:border-slate-600 p-6 rounded-lg">
+                    {(progressionsByCourse[courseTitle].inProgress.length > 0 ||
+                        progressionsByCourse[courseTitle].completed.length > 0) && (
+                        <Link
+                            className="flex gap-2 items-center"
+                            href={`/course/${
+                                progressionsByCourse[courseTitle].inProgress[0]?.lesson.course.id ||
+                                progressionsByCourse[courseTitle].completed[0]?.lesson.course.id
+                            }`}
+                        >
+                            <LibraryBigIcon className="dark:text-white" size={20} />
+                            <span className="font-bold text-2xl dark:text-white">{courseTitle}</span>
+                        </Link>
+                    )}
 
-                            {/* Chapitres en cours pour chaque cours */}
-                            {progressionsByCourse[courseTitle].inProgress.length > 0 && (
-                                <>
-                                    <h4 className="text-md font-semibold text-yellow-600 dark:text-yellow-400 my-5">Chapitres en cours</h4>
-                                    {renderTable(progressionsByCourse[courseTitle].inProgress)}
-                                    {renderResponsive(progressionsByCourse[courseTitle].inProgress)}
-                                </>
-                            )}
+                    {progressionsByCourse[courseTitle].inProgress.length > 0 && (
+                        <>
+                            <h4 className="text-md font-semibold text-yellow-600 dark:text-yellow-400 my-5">Chapitres en cours</h4>
+                            {renderTable(progressionsByCourse[courseTitle].inProgress)}
+                            {renderResponsive(progressionsByCourse[courseTitle].inProgress)}
+                        </>
+                    )}
 
-                            {/* Chapitres terminés pour chaque cours */}
-                            {progressionsByCourse[courseTitle].completed.length > 0 && (
-                                <>
-                                    <h4 className="text-md font-semibold text-green-600 dark:text-green-400 my-5">Chapitres terminés</h4>
-                                    {renderTable(progressionsByCourse[courseTitle].completed)}
-                                    {renderResponsive(progressionsByCourse[courseTitle].completed)}
-                                </>
-                            )}
-                        </div>
-                    ))}
+                    {progressionsByCourse[courseTitle].completed.length > 0 && (
+                        <>
+                            <h4 className="text-md font-semibold text-green-600 dark:text-green-400 my-5">Chapitres terminés</h4>
+                            {renderTable(progressionsByCourse[courseTitle].completed)}
+                            {renderResponsive(progressionsByCourse[courseTitle].completed)}
+                        </>
+                    )}
+                    </div>
+                ))}
                 </div>
             </div>
         </div>
