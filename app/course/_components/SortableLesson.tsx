3fb5da0d554fa79240060000f5cbabc5d2dc1c6f
Micka <mickael.murmann@gmail.com>
2024-10-21 16:25:05 +0200

    Fix CSS

--- app/course/_components/SortableLesson.tsx
+++ app/course/_components/SortableLesson.tsx
@@ -68,7 +68,7 @@ const SortableLesson: React.FC<SortableLessonProps> = ({ lesson }) => {
                                 router.push(`/admin/lesson/delete/${lesson.id}`);
                             }}
                             // className="flex items-center bg-gradient-to-r from-red-500 to-red-600 text-white px-4 py-2 rounded-lg shadow-md"
-                            className="flex items-center text-red-500 hover:bg-slate-700/80 p-2"
+                            className="flex items-center text-red-500 hover:bg-slate-300 dark:hover:bg-slate-700/80 p-2"
                         >
                             <Trash2Icon className="" size={18} />
                         </button>
@@ -77,7 +77,7 @@ const SortableLesson: React.FC<SortableLessonProps> = ({ lesson }) => {
                                 router.push(`/admin/lesson/edit/${lesson.id}`);
                             }}
                             // className="flex items-center bg-gradient-to-r from-blue-500 to-purple-600 text-white px-4 py-2 rounded-lg shadow-md"
-                            className="flex items-center text-slate-900 dark:text-slate-200 hover:bg-slate-700/80 p-2"
+                            className="flex items-center text-slate-900 dark:text-slate-200 hover:bg-slate-300 dark:hover:bg-slate-700/80 p-2"
                         >
                             <EditIcon className="" size={18} />
                         </button>
