97e229a02e7ea11bf0e1078db372987bf5cf587d
Micka <mickael.murmann@gmail.com>
2024-10-23 08:55:59 +0200

    Fix CSS (CategoryCard)

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -9,8 +9,8 @@ interface CategoryCardProps {
 const CategoryCard = ({ id, name }: CategoryCardProps) => {
     return (
         <Link href={`/category/${id}`} className="">
-        <div className="p-6 bg-white dark:bg-gray-800 dark:hover:bg-gray-700 transition duration-300 shadow-md rounded-lg flex flex-col items-center text-center">
-            <h3 className="text-xl text-gray-800 dark:text-gray-100 mb-2">
+        <div className="px-8 py-6 bg-white dark:bg-gray-800 dark:hover:bg-gray-700 transition duration-300 shadow-md rounded-lg flex flex-col items-center text-center">
+            <h3 className="text-xl text-gray-800 dark:text-gray-100">
                 {name}
             </h3>
         </div>
